5c0103944997d251ed05a5e7db4b0d079502cab6
Omer Gillani <sogillani@gmail.com>
2024-05-09 14:11:56 +0200

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bycryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signUp = async (req, res, next) => {
     const { username, email, password} = req.body;
@@ -15,4 +17,28 @@ export const signUp = async (req, res, next) => {
     } catch(error) {
         next(error);
     }
+}
+
+export const signIn = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) {
+            return next(errorHandler(404, "User not found!"));
+        }
+
+        const validPassword = bycryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, "Wrong credentials!"));
+        }
+        const {password: pass, ...rest} = validUser._doc;
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        res.cookie('access_token', token, {httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60)})
+            .status(200)
+            .json(rest);
+
+    } catch(error) {
+        next(error);
+    }
+
 }
